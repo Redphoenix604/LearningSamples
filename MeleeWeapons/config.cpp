27a09f35d78435c6049f1427ab999b417b69973f
RedPhoenix <34327046+Redphoenix604@users.noreply.github.com>
2021-08-02 12:59:05 -0700

    Update config.cpp

--- MeleeWeapons/config.cpp
+++ MeleeWeapons/config.cpp
@@ -1,1 +1,52 @@
+class CfgPatches
+{
+	class PhoenixWarAxe
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data",
+			"DZ_Characters",
+			"DZ_Characters_headgear"
+		};
+	};
+};
+class CfgMods
+{
+	class PhoenixWarAxe
+	{
+		dir="PhoenixWarAxe";
+		picture="";
+		action="";
+		hideName=1;
+		hidePicture=1;
+		name="PhoenixWarAxe";
+		credits="RedPhoenix";
+		author="RedPhoenix";
+		authorID="0";
+		version="1.0";
+		extra=0;
+		type="mod";
+		dependencies[]=
+		{
+			""
 
+		};
+	};
+};
+
+
+
+class cfgVehicles
+{
+	class WoodAxe;
+	class RP_WarAxe: WoodAxe
+	{
+		scope = 2;
+		displayName = "War Axe";
+		descriptionShort = "War Axe made from phoenix ashes.";
+		model = "PhoenixMelee\Models\waraxe_m.p3d";
+	};
+};
